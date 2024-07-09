9e44b378c87b678ee7a9ee419c6bf577e11a1015
Tamannahbot2koyeb <bottamanna2@gmail.com>
2024-07-09 14:19:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '26057782'))
+API_HASH = environ.get('API_HASH', '8fddebc39c1efb6bb72472d87618c59b')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings

d1fb7747e9264e16c0f9bda58fff8abfc507e8c6
Tamannahbot2koyeb <bottamanna2@gmail.com>
2024-07-12 12:33:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -92,8 +92,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Shortlink Info
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
+SHORTLINK_API = environ.get('SHORTLINK_API', '5352f80264414185c873d32d817d1b9b34b0912f')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
